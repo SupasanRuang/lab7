c15b7db15b97e996b9e586abcb5089b538a0f314
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-11-30 15:41:10 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,50 @@
 
 using namespace std;
 
+int adiff(int a,int b)
+{
+  int x;
+    a=a%360;
+    b=b%360;
+    if(a<0)
+    {
+      a=360+a;
+    }
+    if(b<0)
+    {
+      b=360+b;
+    }
+    if(a>b)
+    {
+      x=a-b;
+    }
+    else 
+    {
+      x=b-a;
+    }
+    if(x>180)
+    {
+        return 360-x;
+    }
+    else 
+    {
+        return x;
+    }
+
+}
+
+
+
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  int k;
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
+  cout << adiff(60,244)<<endl;
+  
 }
